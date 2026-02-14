32426c2d2616435565cd99d2c88bdc2f6fa53034
Ranjita Patel <2025ab05117@wilp.bits-pilani.ac.in>
2026-02-14 16:15:21 +0530

    model and model_saved

--- model/knn.py
+++
@@ -1,6 +0,0 @@
-from sklearn.neighbors import KNeighborsClassifier
-
-def train(X_train, y_train):
-    model = KNeighborsClassifier(n_neighbors=5)
-    model.fit(X_train, y_train)
-    return model
--- model/naive_bayes.py
+++
@@ -1,6 +0,0 @@
-from sklearn.naive_bayes import GaussianNB
-
-def train(X_train, y_train):
-    model = GaussianNB()
-    model.fit(X_train, y_train)
-    return model
